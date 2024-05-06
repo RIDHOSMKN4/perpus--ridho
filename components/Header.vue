b93c0ba5707443e60a02f985afedd4ae3e2d8b0b
RIDHOSMKN4 <142279856+RIDHOSMKN4@users.noreply.github.com>
2024-05-06 11:17:08 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="row p-3 bg-primary text-white">
+      <div class="col-lg-1 ">
+        <img src="~/assets/img/LOGO-SMK4.png" width="140" alt="logo SMKN4">
+      </div>
+
+      <div class="col-lg-11">
+        <h1>PERPUS DIGITAL</h1>
+        <address>
+          <div>SMKN4 TASIKMALAYA  </div>
+          jl.depok,sukamenak,purbaratu
+        </address>
+      </div>
+    </div>
+</template>
+
